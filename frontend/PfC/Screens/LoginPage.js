0137dc2315deccdc217d577322bf78767773e783
KalingaDJ <89300552+IT21252990@users.noreply.github.com>
2023-10-21 21:17:54 +0530

    done Login page

--- frontend/PfC/Screens/LoginPage.js
+++ frontend/PfC/Screens/LoginPage.js
@@ -1,102 +1,235 @@
-import React  , {useEffect } from "react";
-import { View, Text, Image, StyleSheet, TextInput } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+  Dimensions,
+  Button,
+} from "react-native";
 import Colors from "../assets/colors/colors";
 import { useNavigation } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 const LoginPage = () => {
-
-   
+  const [isPasswordShown, setIsPasswordShown] = useState(false);
 
   return (
-    <>
-    <Image source={require("../assets/images/mainbg.jpeg")} style = {styles.backgroundPic}/>
-    <View style={styles.loginPageContainer}>
-      <Text style={styles.welcomeText}>Wellcome Back ...</Text>
-      <Image source={require("../assets/images/loginpic.png")} style = {styles.loginPic}/>
-      <View>
-        <Image source={require("../assets/images/loginCard.png")} style = {styles.loginCard}/>
-      <Text style={styles.loginTitle}>LOGIN</Text>
-      <View style={{ marginBottom: 12 }}>
-            <Text
-              style={{
-                fontSize: 16,
-                fontWeight: 400,
-                marginVertical: 8,
-              }}
-            >
-              Username :
-            </Text>
-
-            <View
-              style={{
-                width: "100%",
-                height: 48,
-                borderColor: Colors.primary,
-                borderWidth: 1,
-                borderRadius: 8,
-                alignItems: "center",
-                justifyContent: "center",
-                paddingLeft: 22,
-              }}
-            >
-              <TextInput
-                placeholder="Enter your Username"
-                placeholderTextColor={Colors.primary}
-                // value={email}
-                // onChangeText={(text) => setEmail(text)}
-                keyboardType="default"
-                style={{
-                  width: "100%",
-                }}
-              />
+    <KeyboardAwareScrollView
+      contentContainerStyle={{
+        height: Dimensions.get("window").height * 1.05,
+        width: "100%",
+      }}
+    >
+      <Image
+        source={require("../assets/images/mainbg.jpeg")}
+        style={styles.backgroundPic}
+      />
+      <View style={styles.loginPageContainer}>
+        <Text style={styles.welcomeText}>Welcome Back ...</Text>
+        <Image
+          source={require("../assets/images/loginpic.png")}
+          style={styles.loginPic}
+        />
+          <View>
+            <Image
+              source={require("../assets/images/loginCard.png")}
+              style={styles.loginCard}
+            />
+            <Text style={styles.loginTitle}>LOGIN</Text>
+            <View style={styles.username}>
+              <Text style={styles.usernameLabel}>Username:</Text>
+              <View style={styles.usernameInput}>
+                <Image
+                  source={require("../assets/images/icon-user-dark.png")}
+                  style={styles.inputImage}
+                />
+                <TextInput
+                  placeholder="Enter your Username"
+                  placeholderTextColor={Colors.scondory}
+                  keyboardType="default"
+                  style={styles.input}
+                />
+              </View>
             </View>
+            <View style={styles.password}>
+              <Text style={styles.usernameLabel}>Password:</Text>
+              <View style={styles.usernameInput}>
+                <Image
+                  source={require("../assets/images/icon-password-dark.png")}
+                  style={styles.inputImage}
+                />
+                <TextInput
+                  placeholder="Enter your Password"
+                  placeholderTextColor={Colors.scondory}
+                  secureTextEntry={!isPasswordShown}
+                  keyboardType="default"
+                  style={styles.input}
+                />
+                <TouchableOpacity
+                  onPress={() => setIsPasswordShown(!isPasswordShown)}
+                  style={{
+                    position: "absolute",
+                    right: 12,
+                  }}
+                >
+                  {isPasswordShown == true ? (
+                    <Image
+                      source={require("../assets/images/icon-eye-off.png")}
+                      style={{}}
+                    />
+                  ) : (
+                    <Image
+                      source={require("../assets/images/icon-eye.png")}
+                      style={{}}
+                    />
+                  )}
+                </TouchableOpacity>
+              </View>
+            </View>
+            <TouchableOpacity style={styles.forgotpasswordText}>
+              <Text style={styles.forgotpassword}>Forgot Password ?</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.loginBtnContainer}>
+              <Text style={styles.btnLogin}>
+                LOGIN
+              </Text>
+            </TouchableOpacity> 
+          </View>
+          <View style={styles.linktoSignup}>
+            <Text style={styles.signuplickdis}>Don't have an Account ?   </Text>
+            <TouchableOpacity style={styles.signuptouch}>
+              <Text style={styles.signup}>SIGN UP</Text>
+            </TouchableOpacity>
           </View>
       </View>
-      
-    </View>
-    
-    </>
-    
+    </KeyboardAwareScrollView>
   );
 };
 
 export default LoginPage;
 
 const styles = StyleSheet.create({
-    loginPageContainer:{
-        flex:1,
-        margin:0,
-        position:"absolute"
+  loginPageContainer: {
+    flex: 1,
+    margin: 0,
+    position: "absolute",
+  },
+  backgroundPic: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+  },
+  welcomeText: {
+    color: Colors.scondory,
+    fontSize: 40,
+    position: "absolute",
+    marginTop: 50,
+    marginLeft: 20,
+  },
+  loginPic: {
+    width: 200,
+    height: 200,
+    position: "absolute",
+    marginTop: 90,
+    marginLeft: 80,
+  },
+  loginCard: {
+    marginLeft: 15,
+    marginTop: 280,
+  },
+  loginTitle: {
+    position: "absolute",
+    fontSize: 40,
+    fontWeight: "bold",
+    color: Colors.scondory,
+    marginLeft: 140,
+    marginTop: 300,
+  },
+  usernameLabel: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginVertical: 8,
+  },
+  username: {
+    position: "absolute",
+    marginTop: 380,
+    marginLeft: 60,
+  },
+  password: {
+    position: "absolute",
+    marginTop: 480,
+    marginLeft: 60,
+  },
+  usernameInput: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: 270,
+  },
+  inputImage: {
+    width: 30,
+    height: 30,
+    marginRight: 8,
+  },
+  input: {
+    flex: 1,
+    height: 50,
+    borderColor: Colors.ternary,
+    borderWidth: 1,
+    borderRadius: 20,
+    paddingLeft: 22,
+    fontSize: 16,
+    shadowColor: "#000",
+    shadowRadius: 5,
+    shadowOffset: {
+      width: 10,
+      height: 5,
     },
-    backgroundPic:{
-        width:"100%",
-        height:"100%",
-        position:"relative"
-    },
-    welcomeText:{
-        color:Colors.scondory,
-        fontSize:40,
-        position:"absolute",
-        marginTop:50,
-        marginLeft:20
-    },
-    loginPic:{
-        width:200,
-        height:200,
-        position:"absolute",
-        marginTop:90,
-        marginLeft:80
-    },
-    loginCard:{
-        marginLeft:15,
-        marginTop:280,
-    },
-    loginTitle:{
-        position:"absolute",
-        fontSize:40,
-        fontWeight:"bold",
-        color:Colors.scondory,
-        marginLeft:140,
-        marginTop:300
-    },
+    shadowOpacity: 0.8,
+    backgroundColor: Colors.ternary,
+  },
+  forgotpasswordText: {
+    position: "absolute",
+    marginTop: 580,
+    marginLeft: 230,
+  },
+  forgotpassword: {
+    color: Colors.scondory,
+    textDecorationLine: "underline",
+  },
+  loginBtnContainer:{
+    position:"absolute",
+    marginTop:670,
+    width:180,
+    height:40,
+    borderRadius:20,
+    backgroundColor:Colors.Btn_Positive,
+    justifyContent:"center",
+    alignSelf:"center",
+  },
+  btnLogin:{
+    textAlign:"center",
+    color:Colors.scondory,
+    fontSize:20,
+    fontWeight:"bold"
+  },
+  linktoSignup:{
+    flexDirection:"row",
+    alignSelf:"center",
+    marginTop:7
+  },
+  signuplickdis:{
+    fontSize:16,
+    color:Colors.scondory
+  },
+  signup:{
+    fontSize:18,
+    color:Colors.scondory,
+    fontWeight:"bold",
+    textDecorationLine:"underline"
+  },
 });
--- frontend/PfC/assets/colors/colors.js
+++ frontend/PfC/assets/colors/colors.js
@@ -1,6 +1,8 @@
 const Colors = {
     primary: '#D4B9FE' ,  // light purple (Loading Page bg)
     scondory: "#3D3836",   // main button color / text color
+    ternary: "#FFD2A6", //textinput color / light orange
+    Btn_Positive: '#957AB0' // positive button color / dark purple
    
 }
 
